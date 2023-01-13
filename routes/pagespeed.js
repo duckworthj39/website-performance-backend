a93a2aba1597be619f2a9439acd9eb723e9bda4b
Jason Duckworth <duckworthj39@gmail.com>
2023-01-13 19:52:10 +0000

    changed response structure

--- routes/pagespeed.js
+++ routes/pagespeed.js
@@ -1,18 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
 
 router.get('/', (req, res) => {
-  // const url = req.query.url;
-  let url = decodeURIComponent(req.query.url);
-  // your code here
-  // `https://pagespeed.web.dev/api/runPagespeed?url=${url}`
-  request.get(`https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${url}`, (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-      const data = JSON.parse(body)
-      // send the data to the frontend
+  console.log("test")
+  const competitors = ['https://www.amazon.com', 'https://www.walmart.com', 'https://www.bestbuy.com', 'https://www.target.com'];
+  const promises = competitors.map(url => fetchPageSpeedData(url));
+
+  Promise.all(promises)
+    .then(data => {
       res.json(data);
-    }
-  });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: 'Error fetching data' });
+    });
 });
+
+const fetchPageSpeedData = (url) => {
+  const pagespeedUrl = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${url}`;
+  return new Promise((resolve, reject) => {
+    request(pagespeedUrl, (error, response, body) => {
+      if (error) reject(error);
+      resolve(formatData(JSON.parse(body), url));
+    });
+  });
+}
+
+const formatData = (data, url) => {
+  const speedScore = data.lighthouseResult.categories.performance.score;
+  const firstContentfulPaint = data.lighthouseResult.audits['first-contentful-paint'].displayValue;
+  const timeToInteractive = data.lighthouseResult.audits['interactive'].displayValue;
+
+  return {
+    site: url,
+    speedScore: speedScore,
+    firstContentfulPaint: firstContentfulPaint,
+    timeToInteractive: timeToInteractive,
+  };
+}
 
 module.exports = router;
